60f6acc851b505b89b1355e561541ad4f54cf944
ShadowCurse <shadowcurse57@gmail.com>
2022-09-04 00:31:32 +0300

    added selection of vertical and horizontal layers of the cube

--- src/rubiks_cube.rs
+++ src/rubiks_cube.rs
@@ -25,11 +25,59 @@ struct SubCubeMaterials {
 struct CurrentlySelectedSubCube(Option<Entity>);
 
 #[derive(Component, Debug, Default, Clone, Copy, PartialEq, Eq)]
-struct SubCube(u32);
+struct SubCube(usize);
 
 #[derive(Component, Debug, Clone)]
 struct RubiksCube {
     side_size: u32,
+    cubes: Vec<Entity>,
+}
+
+impl RubiksCube {
+    pub fn select_vertical(&self, index: usize) -> Vec<Entity> {
+        let layer_size = self.side_size.pow(2);
+        let vert_layer = self.vertical_layer(index);
+        ((vert_layer * layer_size)..((vert_layer + 1) * layer_size))
+            .map(|i| self.cubes[i as usize])
+            .collect()
+    }
+
+    pub fn select_horizontal(&self, index: usize) -> Vec<Entity> {
+        let hor_layer = self.horizontal_layer(index);
+        (0..self.side_size)
+            .flat_map(|vert_layer| {
+                ((hor_layer * self.side_size) + vert_layer * self.side_size.pow(2)
+                    ..(hor_layer * self.side_size + self.side_size)
+                        + vert_layer * self.side_size.pow(2))
+                    .map(|i| self.cubes[i as usize])
+            })
+            .collect()
+    }
+
+    /// return vertical cube layer from 0 to side_size
+    fn vertical_layer(&self, index: usize) -> u32 {
+        let layer_size = self.side_size.pow(2);
+        for i in 0..self.side_size {
+            if (index as u32) <= layer_size * (i + 1) {
+                return i;
+            }
+        }
+        unreachable!()
+    }
+
+    /// return horizontal cube layer from 0 to side_size
+    fn horizontal_layer(&self, index: usize) -> u32 {
+        let vert_layer = self.vertical_layer(index);
+        for i in 0..self.side_size {
+            if (i * self.side_size + 1) + vert_layer * self.side_size.pow(2) <= (index as u32)
+                && (index as u32)
+                    <= (i * self.side_size + self.side_size) + vert_layer * self.side_size.pow(2)
+            {
+                return i;
+            }
+        }
+        unreachable!()
+    }
 }
 
 fn setup(
@@ -42,6 +90,7 @@ fn setup(
     }));
     let sub_cube_selected_material = materials.add(Color::GREEN.into());
     let sub_cube_not_selected_material = materials.add(Color::WHITE.into());
+    let mut sub_cubes = Vec::new();
     commands
         .spawn_bundle((
             Transform::default(),
@@ -58,16 +107,6 @@ fn setup(
             for x in 0..CUBE_SIDES {
                 for y in 0..CUBE_SIDES {
                     for z in 0..CUBE_SIDES {
-                        // skipping insides
-                        if x > 0
-                            && x < CUBE_SIDES - 1
-                            && y > 0
-                            && y < CUBE_SIDES - 1
-                            && z > 0
-                            && z < CUBE_SIDES - 1
-                        {
-                            continue;
-                        }
                         let index = CUBE_SIDES * CUBE_SIDES * x + CUBE_SIDES * y + z + 1;
                         let entity = builder
                             .spawn_bundle(PbrBundle {
@@ -80,14 +119,16 @@ fn setup(
                                 ),
                                 ..default()
                             })
-                            .insert(SubCube(index))
+                            .insert(SubCube(index as usize))
                             .id();
+                        sub_cubes.push(entity);
                     }
                 }
             }
         })
         .insert(RubiksCube {
             side_size: CUBE_SIDES,
+            cubes: sub_cubes,
         });
 
     commands.insert_resource(SubCubeMaterials {
@@ -141,5 +182,114 @@ fn sub_cube_selection(
         }
 
         currently_selected_sub_cube.0 = newly_selected;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn generate_rb(sides: u32) -> RubiksCube {
+        let mut sub_cubes = Vec::new();
+        for x in 0..sides {
+            for y in 0..sides {
+                for z in 0..sides {
+                    let index = sides * sides * x + sides * y + z + 1;
+                    let entity = Entity::from_raw(index);
+                    sub_cubes.push(entity);
+                }
+            }
+        }
+        RubiksCube {
+            side_size: sides,
+            cubes: sub_cubes,
+        }
+    }
+
+    #[test]
+    fn vertical_layer() {
+        let rb = generate_rb(3);
+        for i in 1..10 {
+            assert_eq!(rb.vertical_layer(i), 0);
+        }
+        for i in 10..19 {
+            assert_eq!(rb.vertical_layer(i), 1);
+        }
+        for i in 19..28 {
+            assert_eq!(rb.vertical_layer(i), 2);
+        }
+    }
+
+    #[test]
+    fn horizontal_layer() {
+        let rb = generate_rb(3);
+        let layers = [
+            ([1, 2, 3], 0),
+            ([4, 5, 6], 1),
+            ([7, 8, 9], 2),
+            ([10, 11, 12], 0),
+            ([13, 14, 15], 1),
+            ([16, 17, 18], 2),
+            ([19, 20, 21], 0),
+            ([22, 23, 24], 1),
+            ([25, 26, 27], 2),
+        ];
+        for (cubes, layer) in layers {
+            for c in cubes {
+                assert_eq!(rb.horizontal_layer(c), layer);
+            }
+        }
+    }
+
+    #[test]
+    fn select_vertical_layer() {
+        let rb = generate_rb(3);
+        for i in 1..10 {
+            assert_eq!(
+                rb.select_vertical(i),
+                vec![1, 2, 3, 4, 5, 6, 7, 8, 9]
+                    .into_iter()
+                    .map(Entity::from_raw)
+                    .collect::<Vec<_>>()
+            );
+        }
+        for i in 10..19 {
+            assert_eq!(
+                rb.select_vertical(i),
+                vec![10, 11, 12, 13, 14, 15, 16, 17, 18]
+                    .into_iter()
+                    .map(Entity::from_raw)
+                    .collect::<Vec<_>>()
+            );
+        }
+        for i in 19..28 {
+            assert_eq!(
+                rb.select_vertical(i),
+                vec![19, 20, 21, 22, 23, 24, 25, 26, 27]
+                    .into_iter()
+                    .map(Entity::from_raw)
+                    .collect::<Vec<_>>()
+            );
+        }
+    }
+
+    #[test]
+    fn select_horizontal_layer() {
+        let rb = generate_rb(3);
+        let layers = [
+            [1, 2, 3, 10, 11, 12, 19, 20, 21],
+            [4, 5, 6, 13, 14, 15, 22, 23, 24],
+            [7, 8, 9, 16, 17, 18, 25, 26, 27],
+        ];
+        for layer in layers {
+            let entity_layer = layer
+                .iter()
+                .cloned()
+                .map(Entity::from_raw)
+                .collect::<Vec<_>>();
+            for l in layer {
+                assert_eq!(rb.select_horizontal(l as usize), entity_layer);
+            }
+        }
     }
 }
