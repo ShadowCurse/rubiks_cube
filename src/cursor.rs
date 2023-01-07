69e3dfb8b1688a60053c220441bf7e819e568e72
ShadowCurse <shadowcurse57@gmail.com>
2023-01-07 20:16:36 +0000

    collinear axis fix

--- src/cursor.rs
+++ src/cursor.rs
@@ -9,7 +9,7 @@ impl Plugin for CursorRayPlugin {
         app.add_system(cursor_selection_vector);
         app.add_system(selection_vector_colliniar_axis);
 
-        // app.add_startup_system(debug_axis);
+        app.add_startup_system(debug_axis);
         // app.add_system(debug_ray);
     }
 }
@@ -17,14 +17,14 @@ impl Plugin for CursorRayPlugin {
 #[derive(Resource, Debug, Default)]
 pub struct CursorRay(pub Ray);
 
-#[derive(Debug, Default)]
+#[derive(Debug, Default, Clone, Copy)]
 pub struct ScreenVector {
     start: Vec2,
     end: Vec2,
 }
 
 impl ScreenVector {
-    fn vec(&self) -> Vec2 {
+    pub fn vec(&self) -> Vec2 {
         self.end - self.start
     }
 }
@@ -95,44 +95,46 @@ fn selection_vector_colliniar_axis(
 ) {
     if let Ok((camera, camera_transform)) = camera.get_single() {
         if let Some(ref vec) = crs_vector.0 {
-            let scr_vec = vec.vec();
-            if scr_vec.length_squared() < 20.0 {
-                return;
-            }
-            let x_collinearity = camera
-                .world_to_viewport(camera_transform, Vec3::X)
-                .map(|x_axis| scr_vec.dot(x_axis))
-                .unwrap_or(f32::MAX);
-            let y_collinearity = camera
-                .world_to_viewport(camera_transform, Vec3::Y)
-                .map(|y_axis| scr_vec.dot(y_axis))
-                .unwrap_or(f32::MAX);
-            let z_collinearity = camera
-                .world_to_viewport(camera_transform, Vec3::Z)
-                .map(|z_axis| scr_vec.dot(z_axis))
-                .unwrap_or(f32::MAX);
+            if let (Some(ray_start), Some(ray_end)) = (
+                camera.viewport_to_world(camera_transform, vec.start),
+                camera.viewport_to_world(camera_transform, vec.end),
+            ) {
+                let vec = ray_end.origin - ray_start.origin;
+                // have some room for the mouse
+                if vec.length_squared() < 1e-8 {
+                    return;
+                }
 
-            let (colliniarity, axis, neg_axis) = [
-                (x_collinearity, Vec3::X, Vec3::NEG_X),
-                (y_collinearity, Vec3::Y, Vec3::NEG_Y),
-                (z_collinearity, Vec3::Z, Vec3::NEG_Z),
-            ]
-            .into_iter()
-            .max_by(|(c1, _, _), (c2, _, _)| {
-                c1.abs()
-                    .partial_cmp(&c2.abs())
-                    .unwrap_or(std::cmp::Ordering::Equal)
-            })
-            .unwrap();
-            let axis = if colliniarity.is_sign_positive() {
-                axis
+                let vec = vec.normalize();
+                let x_collinearity = vec.dot(Vec3::X);
+                let y_collinearity = vec.dot(Vec3::Y);
+                let z_collinearity = vec.dot(Vec3::Z);
+
+                let (colliniarity, axis, neg_axis) = [
+                    (x_collinearity, Vec3::X, Vec3::NEG_X),
+                    (y_collinearity, Vec3::Y, Vec3::NEG_Y),
+                    (z_collinearity, Vec3::Z, Vec3::NEG_Z),
+                ]
+                .into_iter()
+                .max_by(|(c1, _, _), (c2, _, _)| {
+                    c1.abs()
+                        .partial_cmp(&c2.abs())
+                        .unwrap_or(std::cmp::Ordering::Equal)
+                })
+                .unwrap();
+                let axis = if colliniarity.is_sign_positive() {
+                    axis
+                } else {
+                    neg_axis
+                };
+                crs_colliniar_axis.0 = Some(axis);
             } else {
-                neg_axis
-            };
-            crs_colliniar_axis.0 = Some(axis);
+                crs_colliniar_axis.0 = None;
+            }
         } else {
             crs_colliniar_axis.0 = None;
         }
+        // println!("crs_colliniar_axis: {crs_colliniar_axis:?}");
     }
 }
 
