a2f026b78345a821c233e05a34f568f341249ead
ShadowCurse <shadowcurse57@gmail.com>
2023-01-18 23:20:08 +0000

    updated axis of rotation and layer selection

--- src/rubiks_cube.rs
+++ src/rubiks_cube.rs
@@ -64,11 +64,11 @@ impl RubiksCube {
     pub fn select_rotation(&self, cube_id: usize, rotation_axis: Vec3) -> Vec<u32> {
         let cube_pos = self.cube_to_pos[cube_id];
         let (x, y, z) = self.pos_to_qube_coords(cube_pos);
-        if rotation_axis == Vec3::X || rotation_axis == Vec3::NEG_X {
+        if rotation_axis == Vec3::X {
             self.select_x_layer(x)
-        } else if rotation_axis == Vec3::Y || rotation_axis == Vec3::NEG_Y {
+        } else if rotation_axis == Vec3::Y {
             self.select_y_layer(y)
-        } else if rotation_axis == Vec3::Z || rotation_axis == Vec3::NEG_Z {
+        } else if rotation_axis == Vec3::Z {
             self.select_z_layer(z)
         } else {
             unreachable!("Axis of rotation should only be a unit base vector: {rotation_axis}");
@@ -78,9 +78,9 @@ impl RubiksCube {
     pub fn select_axis_and_rotation(normal: Vec3, direction: Vec3) -> (Vec3, Rotation) {
         let cross = normal.cross(direction);
         if cross.x < 0.0 || cross.y < 0.0 || cross.z < 0.0 {
-            (cross, Rotation::Cw)
+            (cross.abs(), Rotation::Cw)
         } else {
-            (cross, Rotation::Ccw)
+            (cross.abs(), Rotation::Ccw)
         }
     }
 
@@ -108,14 +108,14 @@ impl RubiksCube {
     }
 
     pub fn select_y_layer(&self, y: u32) -> Vec<u32> {
-        (0..self.side_size)
+        (0..self.side_size).rev()
             .flat_map(|x| (0..self.side_size).map(move |z| self.cube_corrds_to_pos(x, y, z)))
             .collect()
     }
 
     pub fn select_z_layer(&self, z: u32) -> Vec<u32> {
         (0..self.side_size)
-            .flat_map(|x| (0..self.side_size).map(move |y| self.cube_corrds_to_pos(x, y, z)))
+            .flat_map(|y| (0..self.side_size).rev().map(move |x| self.cube_corrds_to_pos(x, y, z)))
             .collect()
     }
 }
@@ -137,7 +137,7 @@ impl Display for RubiksCube {
             for y in 0..self.side_size {
                 for z in 0..self.side_size {
                     let index = self.cube_corrds_to_pos(x, y, z);
-                    write!(f, " {:?} ", self.pos_to_cube[index as usize].0)?;
+                    write!(f, " {:?} ", self.cube_to_pos[index as usize])?;
                 }
                 writeln!(f)?;
             }
@@ -177,11 +177,11 @@ mod tests {
         assert_eq!(rotation, Rotation::Ccw);
 
         let (axis, rotation) = RubiksCube::select_axis_and_rotation(Vec3::X, Vec3::NEG_Y);
-        assert_eq!(axis, Vec3::NEG_Z);
+        assert_eq!(axis, Vec3::Z);
         assert_eq!(rotation, Rotation::Cw);
 
         let (axis, rotation) = RubiksCube::select_axis_and_rotation(Vec3::X, Vec3::Z);
-        assert_eq!(axis, Vec3::NEG_Y);
+        assert_eq!(axis, Vec3::Y);
         assert_eq!(rotation, Rotation::Cw);
 
         let (axis, rotation) = RubiksCube::select_axis_and_rotation(Vec3::X, Vec3::NEG_Z);
@@ -190,7 +190,7 @@ mod tests {
 
         // NEG_X
         let (axis, rotation) = RubiksCube::select_axis_and_rotation(Vec3::NEG_X, Vec3::Y);
-        assert_eq!(axis, Vec3::NEG_Z);
+        assert_eq!(axis, Vec3::Z);
         assert_eq!(rotation, Rotation::Cw);
 
         let (axis, rotation) = RubiksCube::select_axis_and_rotation(Vec3::NEG_X, Vec3::NEG_Y);
@@ -202,12 +202,12 @@ mod tests {
         assert_eq!(rotation, Rotation::Ccw);
 
         let (axis, rotation) = RubiksCube::select_axis_and_rotation(Vec3::NEG_X, Vec3::NEG_Z);
-        assert_eq!(axis, Vec3::NEG_Y);
+        assert_eq!(axis, Vec3::Y);
         assert_eq!(rotation, Rotation::Cw);
 
         // Y
         let (axis, rotation) = RubiksCube::select_axis_and_rotation(Vec3::Y, Vec3::X);
-        assert_eq!(axis, Vec3::NEG_Z);
+        assert_eq!(axis, Vec3::Z);
         assert_eq!(rotation, Rotation::Cw);
 
         let (axis, rotation) = RubiksCube::select_axis_and_rotation(Vec3::Y, Vec3::NEG_X);
@@ -219,7 +219,7 @@ mod tests {
         assert_eq!(rotation, Rotation::Ccw);
 
         let (axis, rotation) = RubiksCube::select_axis_and_rotation(Vec3::Y, Vec3::NEG_Z);
-        assert_eq!(axis, Vec3::NEG_X);
+        assert_eq!(axis, Vec3::X);
         assert_eq!(rotation, Rotation::Cw);
 
         // NEG_Y
@@ -228,11 +228,11 @@ mod tests {
         assert_eq!(rotation, Rotation::Ccw);
 
         let (axis, rotation) = RubiksCube::select_axis_and_rotation(Vec3::NEG_Y, Vec3::NEG_X);
-        assert_eq!(axis, Vec3::NEG_Z);
+        assert_eq!(axis, Vec3::Z);
         assert_eq!(rotation, Rotation::Cw);
 
         let (axis, rotation) = RubiksCube::select_axis_and_rotation(Vec3::NEG_Y, Vec3::Z);
-        assert_eq!(axis, Vec3::NEG_X);
+        assert_eq!(axis, Vec3::X);
         assert_eq!(rotation, Rotation::Cw);
 
         let (axis, rotation) = RubiksCube::select_axis_and_rotation(Vec3::NEG_Y, Vec3::NEG_Z);
@@ -245,11 +245,11 @@ mod tests {
         assert_eq!(rotation, Rotation::Ccw);
 
         let (axis, rotation) = RubiksCube::select_axis_and_rotation(Vec3::Z, Vec3::NEG_X);
-        assert_eq!(axis, Vec3::NEG_Y);
+        assert_eq!(axis, Vec3::Y);
         assert_eq!(rotation, Rotation::Cw);
 
         let (axis, rotation) = RubiksCube::select_axis_and_rotation(Vec3::Z, Vec3::Y);
-        assert_eq!(axis, Vec3::NEG_X);
+        assert_eq!(axis, Vec3::X);
         assert_eq!(rotation, Rotation::Cw);
 
         let (axis, rotation) = RubiksCube::select_axis_and_rotation(Vec3::Z, Vec3::NEG_Y);
@@ -258,7 +258,7 @@ mod tests {
 
         // NEG_Z
         let (axis, rotation) = RubiksCube::select_axis_and_rotation(Vec3::NEG_Z, Vec3::X);
-        assert_eq!(axis, Vec3::NEG_Y);
+        assert_eq!(axis, Vec3::Y);
         assert_eq!(rotation, Rotation::Cw);
 
         let (axis, rotation) = RubiksCube::select_axis_and_rotation(Vec3::NEG_Z, Vec3::NEG_X);
@@ -270,69 +270,79 @@ mod tests {
         assert_eq!(rotation, Rotation::Ccw);
 
         let (axis, rotation) = RubiksCube::select_axis_and_rotation(Vec3::NEG_Z, Vec3::NEG_Y);
-        assert_eq!(axis, Vec3::NEG_X);
+        assert_eq!(axis, Vec3::X);
         assert_eq!(rotation, Rotation::Cw);
     }
 
-    // #[test]
-    // fn rb_rotate_single() {
-    //     let mut rb = generate_rb(3);
-    //     rb.rotate(0, Vec3::NEG_Y, Vec3::Z);
-    //     let expected_cubes_pos = vec![
-    //         6, 3, 0, 7, 4, 1, 8, 5, 2, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23,
-    //         24, 25, 26,
-    //     ]
-    //     .into_iter()
-    //     .map(|c| (Entity::from_raw(c), c as usize))
-    //     .collect::<Vec<_>>();
-    //     assert_eq!(rb.pos_to_cube, expected_cubes_pos);
-    //
-    //     let mut rb = generate_rb(3);
-    //     rb.rotate(0, Vec3::NEG_X, Vec3::Z);
-    //     let expected_cubes_pos = vec![
-    //         18, 9, 0, 3, 4, 5, 6, 7, 8, 19, 10, 1, 12, 13, 14, 15, 16, 17, 20, 11, 2, 21, 22, 23,
-    //         24, 25, 26,
-    //     ]
-    //     .into_iter()
-    //     .map(|c| (Entity::from_raw(c), c as usize))
-    //     .collect::<Vec<_>>();
-    //     assert_eq!(rb.pos_to_cube, expected_cubes_pos);
-    //
-    //     let mut rb = generate_rb(3);
-    //     rb.rotate(0, Vec3::NEG_X, Vec3::Y);
-    //     let expected_cubes_pos = vec![
-    //         18, 1, 2, 9, 4, 5, 0, 7, 8, 21, 10, 11, 12, 13, 14, 3, 16, 17, 24, 19, 20, 15, 22, 23,
-    //         6, 25, 26,
-    //     ]
-    //     .into_iter()
-    //     .map(|c| (Entity::from_raw(c), c as usize))
-    //     .collect::<Vec<_>>();
-    //     assert_eq!(rb.pos_to_cube, expected_cubes_pos);
-    // }
+    #[test]
+    fn rb_rotate_single() {
+        let mut rb = generate_rb(3);
+        rb.rotate(0, Vec3::NEG_Y, Vec3::Z);
+        let expected_cubes_pos = vec![
+            6, 3, 0, 7, 4, 1, 8, 5, 2, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23,
+            24, 25, 26,
+        ]
+        .into_iter()
+        .map(|c| (Entity::from_raw(c), c as usize))
+        .collect::<Vec<_>>();
+        assert_eq!(rb.pos_to_cube, expected_cubes_pos);
 
-    // #[test]
-    // fn rb_rotate_multiple() {
-    //     let mut rb = generate_rb(3);
-    //     rb.rotate(0, Vec3::NEG_Y, Vec3::Z);
-    //     let expected_cubes_pos = vec![
-    //         6, 3, 0, 7, 4, 1, 8, 5, 2, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23,
-    //         24, 25, 26,
-    //     ]
-    //     .into_iter()
-    //     .map(|c| (Entity::from_raw(c), c as usize))
-    //     .collect::<Vec<_>>();
-    //     assert_eq!(rb.pos_to_cube, expected_cubes_pos);
-    //
-    //     rb.rotate(6, Vec3::NEG_X, Vec3::Z);
-    //     let expected_cubes_pos = vec![
-    //         18, 9, 6, 7, 4, 1, 8, 5, 2, 19, 10, 3, 12, 13, 14, 15, 16, 17, 20, 11, 0, 21, 22, 23,
-    //         24, 25, 26,
-    //     ]
-    //     .into_iter()
-    //     .map(|c| (Entity::from_raw(c), c as usize))
-    //     .collect::<Vec<_>>();
-    //     assert_eq!(rb.pos_to_cube, expected_cubes_pos);
-    // }
+        let mut rb = generate_rb(3);
+        rb.rotate(0, Vec3::NEG_X, Vec3::Z);
+        let expected_cubes_pos = vec![
+            18, 9, 0, 3, 4, 5, 6, 7, 8, 19, 10, 1, 12, 13, 14, 15, 16, 17, 20, 11, 2, 21, 22, 23,
+            24, 25, 26,
+        ]
+        .into_iter()
+        .map(|c| (Entity::from_raw(c), c as usize))
+        .collect::<Vec<_>>();
+        assert_eq!(rb.pos_to_cube, expected_cubes_pos);
+
+        let mut rb = generate_rb(3);
+        rb.rotate(0, Vec3::NEG_X, Vec3::Y);
+        let expected_cubes_pos = vec![
+            18, 1, 2, 9, 4, 5, 0, 7, 8, 21, 10, 11, 12, 13, 14, 3, 16, 17, 24, 19, 20, 15, 22, 23,
+            6, 25, 26,
+        ]
+        .into_iter()
+        .map(|c| (Entity::from_raw(c), c as usize))
+        .collect::<Vec<_>>();
+        assert_eq!(rb.pos_to_cube, expected_cubes_pos);
+    }
+
+    #[test]
+    fn rb_rotate_multiple() {
+        let mut rb = generate_rb(3);
+        rb.rotate(0, Vec3::NEG_Y, Vec3::Z);
+        let expected_cubes_pos = vec![
+            6, 3, 0, 7, 4, 1, 8, 5, 2, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23,
+            24, 25, 26,
+        ]
+        .into_iter()
+        .map(|c| (Entity::from_raw(c), c as usize))
+        .collect::<Vec<_>>();
+        assert_eq!(rb.pos_to_cube, expected_cubes_pos);
+
+        rb.rotate(6, Vec3::NEG_X, Vec3::Z);
+        let expected_cubes_pos = vec![
+            18, 9, 6, 7, 4, 1, 8, 5, 2, 19, 10, 3, 12, 13, 14, 15, 16, 17, 20, 11, 0, 21, 22, 23,
+            24, 25, 26,
+        ]
+        .into_iter()
+        .map(|c| (Entity::from_raw(c), c as usize))
+        .collect::<Vec<_>>();
+        assert_eq!(rb.pos_to_cube, expected_cubes_pos);
+
+        rb.rotate(20, Vec3::NEG_Y, Vec3::Z);
+        let expected_cubes_pos = vec![
+            18, 9, 6, 7, 4, 1, 8, 5, 2, 19, 10, 3, 12, 13, 14, 15, 16, 17, 24, 21, 20, 25, 22, 11,
+            26, 23, 0,
+        ]
+        .into_iter()
+        .map(|c| (Entity::from_raw(c), c as usize))
+        .collect::<Vec<_>>();
+        assert_eq!(rb.pos_to_cube, expected_cubes_pos);
+    }
 
     #[test]
     fn rb_rotate_indices() {
@@ -348,18 +358,18 @@ mod tests {
 
         let indices = rb.select_rotation(0, Vec3::Y);
         let rotated = rb.rotate_indices(&indices, Rotation::Cw);
-        let expected = vec![18, 9, 0, 19, 10, 1, 20, 11, 2];
+        let expected = vec![0, 9, 18, 1, 10, 19, 2, 11, 20];
         assert_eq!(rotated, expected);
         let rotated = rb.rotate_indices(&indices, Rotation::Ccw);
-        let expected = vec![2, 11, 20, 1, 10, 19, 0, 9, 18];
+        let expected = vec![20, 11, 2, 19, 10, 1, 18, 9, 0];
         assert_eq!(rotated, expected);
 
         let indices = rb.select_rotation(0, Vec3::Z);
         let rotated = rb.rotate_indices(&indices, Rotation::Cw);
-        let expected = vec![18, 9, 0, 21, 12, 3, 24, 15, 6];
+        let expected = vec![24, 21, 18, 15, 12, 9, 6, 3, 0];
         assert_eq!(rotated, expected);
         let rotated = rb.rotate_indices(&indices, Rotation::Ccw);
-        let expected = vec![6, 15, 24, 3, 12, 21, 0, 9, 18];
+        let expected = vec![0, 3, 6, 9, 12, 15, 18, 21, 24];
         assert_eq!(rotated, expected);
     }
 
@@ -390,7 +400,7 @@ mod tests {
         for y in 0..3 {
             let offset = 3;
             let layer = rb.select_y_layer(y);
-            let expected = vec![0, 1, 2, 9, 10, 11, 18, 19, 20]
+            let expected = vec![18, 19, 20, 9, 10, 11, 0, 1, 2]
                 .into_iter()
                 .map(|v| v + offset * y)
                 .collect::<Vec<_>>();
@@ -399,7 +409,7 @@ mod tests {
         for z in 0..3 {
             let offset = 1;
             let layer = rb.select_z_layer(z);
-            let expected = vec![0, 3, 6, 9, 12, 15, 18, 21, 24]
+            let expected = vec![18, 9, 0, 21, 12, 3, 24, 15, 6]
                 .into_iter()
                 .map(|v| v + offset * z)
                 .collect::<Vec<_>>();
@@ -414,10 +424,10 @@ mod tests {
         let x_expected = vec![0, 1, 2, 3, 4, 5, 6, 7, 8];
         assert_eq!(x_entities, x_expected);
         let y_entities = rb.select_rotation(0, Vec3::Y);
-        let y_expected = vec![0, 1, 2, 9, 10, 11, 18, 19, 20];
+        let y_expected = vec![18, 19, 20, 9, 10, 11, 0, 1, 2];
         assert_eq!(y_entities, y_expected);
         let z_entities = rb.select_rotation(0, Vec3::Z);
-        let z_expected = vec![0, 3, 6, 9, 12, 15, 18, 21, 24];
+        let z_expected = vec![18, 9, 0, 21, 12, 3, 24, 15, 6];
         assert_eq!(z_entities, z_expected);
     }
 }
